bb0c1e6871868e580e71351d2e0f8f05f35a8301
elleom <el_leo_m@hotmail.com>
2021-06-26 00:09:28 +0200

    create skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { AppComponent } from './app.component';
 import {HeaderComponent} from "./header/header.component";
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
