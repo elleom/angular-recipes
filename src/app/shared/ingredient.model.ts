16a802eb4f61cffdec949e7b8fb810b9a97f7eed
elleom <el_leo_m@hotmail.com>
2021-06-29 22:25:54 +0200

    created ingredient model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,16 @@
+export class Ingredient {
+   public name: string;
+   public amount: number;
+
+   constructor(name : string, amount: number){
+     this.name = name;
+     this.amount = amount;
+   }
+
+  /**
+   * alt =ly fine
+   * constructor(public name:string, public amount: number) {}
+   */
+
+
+}
