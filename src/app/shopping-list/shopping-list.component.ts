255b9d7e2f5e91a45a8a580e37d7df1bd330c986
elleom <el_leo_m@hotmail.com>
2021-10-10 21:36:50 +0200

    added Add Item to SL

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -18,4 +18,7 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onIngredientWasAdded(ingredient : Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
