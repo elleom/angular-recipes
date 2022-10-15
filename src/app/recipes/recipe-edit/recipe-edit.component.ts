0fa11b3ff0993970ff93eaf9eb2c8c2b5eb7485b
0el_le0 <el_leo_m@hotmail.com>
2022-10-15 20:58:12 +0200

    navigate after delete unsubscribe cleanUp

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -93,4 +93,9 @@ export class RecipeEditComponent implements OnInit {
   onCancel() {
     this.router.navigate(['../'], {relativeTo: this.route})
   }
+
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index)
+
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,32 +1,38 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Recipe} from "../recipe.model";
 import {RecipeService} from "../recipe.service";
 import {ActivatedRoute, Router} from "@angular/router";
-import {Observable} from "rxjs";
+import {Observable, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
-  recipesObservable: Observable<Recipe[]>
+export class RecipeListComponent implements OnInit, OnDestroy {
+  recipesObservable: Subscription
 
   recipes: Recipe[];
+
   constructor(private recipeService: RecipeService,
               private router: Router,
               private route: ActivatedRoute
-          ) { }
+  ) {
+  }
+
+  ngOnDestroy(): void {
+    this.recipesObservable.unsubscribe()
+  }
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
 
-    this.recipeService.recipesChanged.subscribe((recipesData: Recipe[]) => {
+    this.recipesObservable = this.recipeService.recipesChanged.subscribe((recipesData: Recipe[]) => {
       this.recipes = recipesData;
     })
   }
 
   onNewRecipe() {
-    this.router.navigate(['new'],{ relativeTo: this.route})
+    this.router.navigate(['new'], {relativeTo: this.route})
   }
 }
