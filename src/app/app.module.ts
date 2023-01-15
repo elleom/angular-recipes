74c2193a4996abb00c18f8bbed728acd2f8682f2
0el_le0 <el_leo_m@hotmail.com>
2023-01-15 09:56:25 +0100

    mod error message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AuthComponent} from "./auth/auth.component";
 import {SpinnerComponent} from "./shared/spinner/spinner.component";
 import {AuthInterceptorService} from "./auth/auth.interceptor.service";
+import {AlertComponent} from "./shared/alert/alert.component";
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import {AuthInterceptorService} from "./auth/auth.interceptor.service";
     RecipeStartComponent,
     RecipeEditComponent,
     AuthComponent,
-    SpinnerComponent
+    SpinnerComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
